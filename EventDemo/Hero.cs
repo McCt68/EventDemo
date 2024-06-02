d8c000e69a09b5cd9158a863658938f27426376e
mcct <mcct@privat.dk>
2024-06-02 22:32:42 +0200

    Add project files.

---
+++ EventDemo/Hero.cs
@@ -0,0 +1,36 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+
+namespace EventDemo
+
+// Publisher.
+
+// The publisher is the class that :
+
+// Declares the Event: It defines the event using the event keyword.
+// Raises the Event: It invokes the event, usually when some condition or state change occurs within the class.
+{
+    public class Hero
+    {
+        private int _health;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Health 
+        { 
+            get => this._health;
+            set
+            {
+                this._health = value;
+
+                // Notify subscribers hvilken Hero instance og den nye værdi for _health
+                // this is the instance that sends the event, along with the new health value
+                this.OnHealthChanged?.Invoke(this, _health);
+            }            
+        }
+
+        // Declarer an event - Dette er meget som at declarere en delegate
+        // sender: The first parameter is of type object and refers to the instance that raised the event.
+        // e: The second parameter is of type T (in this case, int), and it contains the event data.
+        public event EventHandler<int> OnHealthChanged;
+    }
+}
---
+++ EventDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EventDemo;
+using System.Reflection.Metadata;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hero event Program\n");
+
+        // Subscriber
+        // The subscriber is any class that :
+
+        // Subscribes to the Event: It attaches event handler methods to the event using the += syntax.
+        // Handles the Event: It defines methods that match the event's delegate signature to handle the event when it is raised.
+
+        Hero myFirstHero = new Hero
+        {
+            Id = 1,
+            Name = "Egon",
+            Health = 100
+        };
+
+        Hero mySecondHero = new Hero
+        {
+            Id = 2,
+            Name = "Benny",
+            Health = 80
+        };
+
+        myFirstHero.OnHealthChanged += HeroOnHealthChanged;  
+        mySecondHero.OnHealthChanged += HeroOnDeath;  
+        
+        myFirstHero.Health = 55;
+        mySecondHero.Health = mySecondHero.Health - 100;
+    }
+
+    private static void HeroOnDeath(object? sender, int e)
+    {
+        var hero = (Hero)sender;
+        Console.WriteLine($"Hero {hero.Name} faldt ned fra en klippe");
+        Console.WriteLine($"Hero {hero.Name} og har nu {hero.Health} liv points og er død\n");
+    }
+
+    public static void HeroOnHealthChanged(object? sender, int e)
+    {
+        // sender er den instans der notificerer mig at dens state der representerer health er ændret
+        var hero = (Hero)sender;
+        Console.WriteLine($"Hero {hero.Name} Fik et slag i maven");
+        Console.WriteLine($"Hero {hero.Name} har nu {hero.Health} livpoints\n");
+    }
+}
